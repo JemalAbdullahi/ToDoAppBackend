585e0045cea422cfe987a4ac5fcc9a98301a375f
Jemal <53106252+Jebdi@users.noreply.github.com>
2021-04-05 20:45:40 -0500

    update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
@@ -8,7 +9,7 @@ print(os.environ['APP_SETTINGS'])
 from api.api import api_bp
 app.register_blueprint(api_bp, url_prefix='/api')
 
-from Models import d
+from Models import db
 db.init_app(app)
 
 
