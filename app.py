7961f68c356ae4b242dac857737313621d42ea2a
Jemal <53106252+Jebdi@users.noreply.github.com>
2021-07-12 12:27:45 -0500

    Privacy1

--- app.py
+++ app.py
@@ -23,10 +23,10 @@ db.init_app(app)
 def index():
     return "<h1>Welcome to our server !!</h1>"
 
+
 @app.route('/privacy%20policy')
 def privacy():
-    return """
-        <h1>Privacy Policy</h1>
+    return """<h1>Privacy Policy</h1>
         <p>Last updated: July 10, 2021</p>
         <p>This Privacy Policy describes Our policies and procedures on the collection, use and disclosure of Your information when You use the Service and tells You about Your privacy rights and how the law protects You.</p>
         <p>We use Your Personal data to provide and improve the Service. By using the Service, You agree to the collection and use of information in accordance with this Privacy Policy. This Privacy Policy has been created with the help of the <a href="https://www.termsfeed.com/privacy-policy-generator/" target="_blank">Privacy Policy Generator</a>.</p>
@@ -167,8 +167,7 @@ def privacy():
         <p>If you have any questions about this Privacy Policy, You can contact us:</p>
         <ul>
         <li>By email: jemal.a.abdullahi@gmail.com</li>
-        </ul>
-    """
+        </ul>"""
 
 
 if __name__ == "__main__":
